bd262dba4ccd95c08ef52b964705aa51c26ecb2f
lildemaran-ui <lildemaran@gmail.com>
2026-03-04 17:24:26 +0100

    feat: refactor menu components and integrate MenuEncar for sidebar navigation

--- src/Pages/Encarregado/ConfiguracaoEncar.tsx
+++ src/Pages/Encarregado/ConfiguracaoEncar.tsx
@@ -1,19 +1,15 @@
-import { Link } from "react-router-dom";
-import Logo5 from "../../assets/Logo5.5.png";
+
 import { useEffect, useState } from "react";
 import {
   Bell,
-  LayoutDashboard,
-  LogOut,
   Menu,
-  MessageSquare,
-  Receipt,
   Settings,
   X,
-  type LucideIcon,
+  
 } from "lucide-react";
 import Avatar from "@/components/Avatar/Avatar";
 import ConfiguracaoGeral from "@/components/Configuracoes/ConfiguracaoGeral";
+import MenuEncar from "@/components/Menu/MenuEncar";
 export default function ConfiguracaoEncar() {
   const [Modal, setModal] = useState(false);
 
@@ -50,100 +46,11 @@ export default function ConfiguracaoEncar() {
   if (!user) {
     return <span>Carregado...</span>;
   }
-  const SidebarItem = ({
-    icon: Icon,
-    label,
-    active = false,
-  }: {
-    icon: LucideIcon;
-    label: string;
-    active?: boolean;
-  }) => (
-    <div
-      className={`flex items-center gap-3 p-3 rounded-lg  ml-3 cursor-pointer transition-colors duration-500 ${
-        active ? "bg-white/20 w-56  " : "hover:bg-white/10 w-56"
-      }`}
-    >
-      <Icon size={22} className="text-white" />
-      <span className="text-white font-medium text-sm">{label}</span>
-    </div>
-  );
+  
   return (
     <div className="flex overflow-hidden h-screen bg-[#f8fafc] font-sans transition-all duration-500">
       {/* Sidebar */}
-
-      {menu && (
-        <aside className="w-64 bg-[#268cff] flex flex-col sticky top-0 h-screen">
-          <div className="px-4 pt-4 mb-10 flex items-center gap-2 relative justify-between">
-            <div className=" flex items-center">
-              <img
-                loading="lazy"
-                src={Logo5}
-                alt="Logo"
-                className="w-16 h-16 "
-              />
-              <p className="text-white font-semibold">ClassCash</p>
-            </div>
-            <button>
-              <Menu size={22} className="text-white" onClick={CloseMenu} />
-            </button>
-          </div>
-
-          <nav className="flex-1 flex flex-col gap-1 text-white">
-            <Link to="/Encarregado">
-              <SidebarItem
-                icon={LayoutDashboard}
-                label="Painel Geral"
-                active={window.location.pathname === "/Encarregado"}
-              />
-            </Link>
-            <Link to="/PagamentoEncar">
-              <SidebarItem
-                icon={Receipt}
-                label="Pagamento"
-                active={window.location.pathname === "/PagamentoEncar"}
-              />
-            </Link>
-
-            <Link to="/DashboardEstud">
-              <SidebarItem
-                icon={Users}
-                label="Painel do seu Educando"
-                active={window.location.pathname === "/DashboardEstud"}
-              />
-            </Link>
-            <Link to="/ReclamacoesEncar">
-              <SidebarItem
-                icon={MessageSquare}
-                label="Reclamações"
-                active={window.location.pathname === "/ReclamacoesEncar"}
-              />
-            </Link>
-            <Link to="/ConfiguracaoEncar">
-              <SidebarItem
-                icon={Settings}
-                label="Definições"
-                active={window.location.pathname === "/ConfiguracaoEncar"}
-              />
-            </Link>
-          </nav>
-          <Link
-            to="/Login"
-            className="hover:bg-blue-400 px-4 rounded-sm  border border-white/10 bg-blue-500/50  transition-all duration-700 p-3 group"
-          >
-            <div className="flex justify-between  items-center ">
-              <span className="text-sm font-medium text-white group-hover:text-blue-700">
-                Terminar sessão
-              </span>
-              <LogOut
-                size={22}
-                className="text-white font-medium group-hover:text-blue-700"
-              />
-            </div>
-          </Link>
-        </aside>
-      )}
-
+      <MenuEncar/>
       {/* Modal de edição de perfil */}
       {Modal && (
         <div className="  bg-translucido2 fixed inset-0 z-50 flex items-center justify-center ">
--- src/components/Menu/MenuEstud.tsx
+++ src/components/Menu/MenuEstud.tsx
@@ -1,56 +1,44 @@
-import { useState } from "react";
-import {
-  LayoutDashboard,
-  Wallet,
-  MessageSquare,
-  Settings,
-  LifeBuoy,
-  Menu,
-  LogOut,
-} from "lucide-react";
-import logo5 from "../../../assets/Logo5.5.png";
+import { LayoutDashboard, LifeBuoy, LogOut, Menu, MessageSquare, Settings, Wallet } from "lucide-react";
 import { Link } from "react-router-dom";
-
-import DadosDashEstd from "./DadosDashEstd";
-export default function DashboardEstud() {
-  const [menu, setMenu] = useState(() => {
-    const saved = localStorage.getItem("menu_aberto");
-    return saved === "true";
-  });
-
-  function OpenMenu() {
-    setMenu(true);
-    localStorage.setItem("menu_aberto", "true");
-  }
-  function CloseMenu() {
-    setMenu(false);
-    localStorage.setItem("menu_aberto", "false");
-  }
-  // Sub-componentes auxiliares
-  const NavItem = ({
-    icon,
-    label,
-    active = false,
-  }: {
-    icon: React.ReactNode;
-    label: string;
-    active?: boolean;
-  }) => (
-    <div
-      className={`flex items-center gap-3 p-3 mt-2 rounded-lg cursor-pointer transition-all duration-300 ${
-        active ? "bg-white/10" : "hover:bg-white/5"
-      }`}
-    >
-      {icon}
-      <span className="text-sm font-medium">{label}</span>
-    </div>
-  );
-
-  return (
-    <div className="flex h-screen overflow-hidden bg-gray-50 font-sans transition-all duration-500">
-      {/* Sidebar */}
-
-      {menu && (
+import logo5 from "../../assets/Logo5.5.png";
+import { useState } from "react";
+export default function MenuEstud () {
+      const [menu, setMenu] = useState(() => {
+        const saved = localStorage.getItem("menu_aberto");
+        return saved === "true";
+      });
+    
+      function OpenMenu() {
+        setMenu(true);
+        localStorage.setItem("menu_aberto", "true");
+      }
+      function CloseMenu() {
+        setMenu(false);
+        localStorage.setItem("menu_aberto", "false");
+      }
+     
+     // Sub-componentes auxiliares
+      const NavItem = ({
+        icon,
+        label,
+        active = false,
+      }: {
+        icon: React.ReactNode;
+        label: string;
+        active?: boolean;
+      }) => (
+        <div
+          className={`flex items-center gap-3 p-3 mt-2 rounded-lg cursor-pointer transition-all duration-300 ${
+            active ? "bg-white/10" : "hover:bg-white/5"
+          }`}
+        >
+          {icon}
+          <span className="text-sm font-medium">{label}</span>
+        </div>
+      );
+    return (
+        <div>
+            {menu && (
         <aside className="w-64 bg-[#268cff] text-white flex flex-col h-sreen top-0 sticky">
           <div className="mb-16 pt-4 flex relative justify-between items-center px-4">
             <div className="flex items-center font-semibold">
@@ -86,14 +74,14 @@ export default function DashboardEstud() {
               <NavItem
                 icon={<MessageSquare size={22} />}
                 label="Reclamações"
-                active={false}
+                active={window.location.pathname === "/reclamacoes"}
               />
             </Link>
             <Link to="/Config">
               <NavItem
                 icon={<Settings size={22} />}
                 label="Configurações"
-                active={false}
+                active={window.location.pathname === "/Config"}
               />
             </Link>
             <NavItem
@@ -116,16 +104,6 @@ export default function DashboardEstud() {
           </Link>
         </aside>
       )}
-
-      {/* Main Content */}
-      <main className="flex-1 p-8 custom_scroll ">
-        {!menu && (
-          <button onClick={OpenMenu}>
-            <Menu size={22} className="text-[#268cff]" />
-          </button>
-        )}
-        <DadosDashEstd />
-      </main>
-    </div>
-  );
+        </div>
+    )
 }
